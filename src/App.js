9e4b2b8c909fc92d3b28ae8b230ef39df9fd6804
trandangquang <trandangquang2203@gmail.com>
2023-10-04 16:13:20 +0700

    call api use react-query

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {routes} from './routes'
-import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import axios from 'axios';
 import { Fragment } from 'react';
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import { routes } from './routes';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+
+  console.log(
+    'process.env.REACT_API_URL_BACKEND',
+    process.env.REACT_APP_API_URL
+  );
+  const fetchApi = async () => {
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/product/get-all`
+    );
+    return res.data
+  };
+
+    const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi });
+    console.log('query', query)
+
   return (
     <div>
       <Router>
         <Routes>
-          {
-            routes.map((route) => {
-              const Page = route.page
-              const LayOut = route.isShowHeader ? DefaultComponent : Fragment
-              return (
-                <Route
-                  path={route.path}
-                  element={
-                    <LayOut>
-                      <Page />
-                    </LayOut>
-                  }
-                />
-              );
-            })
-          }
+          {routes.map((route) => {
+            const Page = route.page;
+            const LayOut = route.isShowHeader ? DefaultComponent : Fragment;
+            return (
+              <Route
+              key={route.path}
+                path={route.path}
+                element={
+                  <LayOut>
+                    <Page />
+                  </LayOut>
+                }
+              />
+            );
+          })}
         </Routes>
       </Router>
     </div>
